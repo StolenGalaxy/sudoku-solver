c1efabf264899d868ac4d97cfdb4336817c1b854
StolenGalaxy <56508206+StolenGalaxy@users.noreply.github.com>
2026-03-12 19:02:59 +0000

    Add more tools for generating random data

--- src/main/java/com/stolengalaxy/sudoku_solver/util/ArrayTools.java
+++ src/main/java/com/stolengalaxy/sudoku_solver/util/ArrayTools.java
@@ -1,6 +1,7 @@
 package com.stolengalaxy.sudoku_solver.util;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ArrayTools {
     public static int countIntegerOccurrences(ArrayList<Integer> array, int targetInteger){
@@ -11,5 +12,38 @@ public class ArrayTools {
             }
         }
         return count;
+    }
+
+    private static ArrayList<Integer> blankArray(int length){
+        ArrayList<Integer> newArray = new ArrayList<>();
+        for(int i = 1; i <= length; i++){
+            newArray.add(0);
+        }
+        return newArray;
+    }
+
+    public static ArrayList<Integer> orderedIntegerArray(int length){
+        ArrayList<Integer> newArray = new ArrayList<>();
+        for(int i=1; i <= length; i++){
+            newArray.add(i);
+        }
+        return newArray;
+    }
+
+    public static ArrayList<Integer> unorderedIntegerArray(int length){
+
+        ArrayList<Integer> array = blankArray(length);
+
+        ArrayList<Integer> unusedValues = orderedIntegerArray(length);
+
+        Random random = new Random();
+        for(int i = 0; i < length; i++){
+
+            int randomListIndex = random.nextInt(unusedValues.size());
+            int randomUnusedValue = unusedValues.get(randomListIndex);
+            unusedValues.remove(randomListIndex);
+            array.set(i, randomUnusedValue);
+        }
+        return array;
     }
 }
