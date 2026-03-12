ab4b800ef78040989bad4362a20eb18530e7cdb3
StolenGalaxy <56508206+StolenGalaxy@users.noreply.github.com>
2026-03-12 19:16:05 +0000

    Add random grid generator
    Note the grid will not be valid

---
+++ src/main/java/com/stolengalaxy/sudoku_solver/grid/Generator.java
@@ -0,0 +1,16 @@
+package com.stolengalaxy.sudoku_solver.grid;
+
+import com.stolengalaxy.sudoku_solver.util.ArrayTools;
+
+import java.util.ArrayList;
+
+public class Generator {
+    private static int size = 9;
+    public static Grid randomGrid(){
+        ArrayList<ArrayList<Integer>> rows = new ArrayList<>();
+        for(int i = 1; i <= 9; i++){
+            rows.add(ArrayTools.unorderedIntegerArray(size));
+        }
+        return new Grid(rows);
+    }
+}
