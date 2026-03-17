70c786f6c30f6811b78c0fc9f9d11b5548651c9a
StolenGalaxy <56508206+StolenGalaxy@users.noreply.github.com>
2026-03-17 19:10:47 +0000

    Add method to get grid as blocks

--- src/main/java/com/stolengalaxy/sudoku_solver/grid/Grid.java
+++ src/main/java/com/stolengalaxy/sudoku_solver/grid/Grid.java
@@ -1,14 +1,22 @@
 package com.stolengalaxy.sudoku_solver.grid;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Grid {
     private ArrayList<ArrayList<Integer>> rows = new ArrayList<>();
     int size;
+    int blockSize;
 
     public Grid(ArrayList<ArrayList<Integer>> rows){
         this.rows = rows;
         this.size = rows.size();
+        double lengthRoot = Math.sqrt(this.size);
+        if(lengthRoot % 1 != 0){
+            throw new RuntimeException("Grid size is invalid");
+        } else {
+            this.blockSize = (int) lengthRoot;
+        }
     }
 
     public ArrayList<ArrayList<Integer>> columns(){
@@ -29,6 +37,45 @@ public class Grid {
 
     public ArrayList<ArrayList<Integer>> rows(){
         return rows;
+    }
+
+    public ArrayList<ArrayList<Integer>> blocks(){
+        ArrayList<ArrayList<Integer>> blocks = new ArrayList<>();
+        for(int i = 1; i <= 9; i++){
+            blocks.add(new ArrayList<>());
+        }
+
+        int rowCount = 0;
+        for(ArrayList<Integer> row:rows){
+            rowCount++;
+            int fromColumn = 0;
+
+            // split the row into 3 (if a standard 9x9 sudoku) sections
+            for(int horizontalBlockIndex = 1; horizontalBlockIndex <= blockSize; horizontalBlockIndex++){
+                for(int i = 1; i <= blockSize; i++){
+                    int targetBlockIndex;
+
+                    //TODO: only works for 9x9 sudoku
+                    if(rowCount <= blockSize){
+                        targetBlockIndex = horizontalBlockIndex - 1;
+                    } else if (rowCount <= 2 * blockSize) {
+                        targetBlockIndex = horizontalBlockIndex + blockSize - 1;
+                    } else if (rowCount <= 3 * blockSize) {
+                        targetBlockIndex = horizontalBlockIndex + 2 * blockSize - 1;
+                    } else{
+                        throw new RuntimeException("Non-standard sudoku not supported.");
+                    }
+                    ArrayList<Integer> currentBlock = blocks.get(targetBlockIndex);
+
+                    currentBlock.add(row.get(fromColumn));
+                    blocks.set(targetBlockIndex, currentBlock);
+                    fromColumn++;
+
+                }
+
+            }
+        }
+        return blocks;
     }
 
     public String toString(){
