6662b426c39b60270b79437a988481a6a097a2d0
StolenGalaxy <56508206+StolenGalaxy@users.noreply.github.com>
2026-03-18 17:52:17 +0000

    Check for occurrences of 0
    0 will be used as an empty space, so presence should mean an uncompleted grid

--- src/main/java/com/stolengalaxy/sudoku_solver/grid/Validation.java
+++ src/main/java/com/stolengalaxy/sudoku_solver/grid/Validation.java
@@ -7,12 +7,19 @@ import java.util.ArrayList;
 public class Validation {
     public static boolean isSetValid(ArrayList<Integer> values){
         boolean stillValid = true;
-        for(int i=1; i <=9; i++){
-            if (ArrayTools.countIntegerOccurrences(values, i) > 1){
-                stillValid = false;
-                break;
+
+        if(ArrayTools.countIntegerOccurrences(values, 0) > 0){
+            stillValid = false;
+        } else{
+            for(int i=1; i <=9; i++){
+                if (ArrayTools.countIntegerOccurrences(values, i) > 1){
+                    stillValid = false;
+                    break;
+                }
             }
         }
+
+
         return stillValid;
     }
 
