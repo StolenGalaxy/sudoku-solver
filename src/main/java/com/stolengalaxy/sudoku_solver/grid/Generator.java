5c11130a13e33ab0cadfefddc3bed7060be22ec8
StolenGalaxy <56508206+StolenGalaxy@users.noreply.github.com>
2026-03-21 18:01:47 +0000

    Add empty grid generator for testing

--- src/main/java/com/stolengalaxy/sudoku_solver/grid/Generator.java
+++ src/main/java/com/stolengalaxy/sudoku_solver/grid/Generator.java
@@ -5,12 +5,26 @@ import com.stolengalaxy.sudoku_solver.util.ArrayTools;
 import java.util.ArrayList;
 
 public class Generator {
-    private static int size = 9;
-    public static Grid randomGrid(){
+    public static Grid randomGrid(int size){
         ArrayList<ArrayList<Integer>> rows = new ArrayList<>();
         for(int i = 1; i <= size; i++){
             rows.add(ArrayTools.unorderedIntegerArray(size));
         }
+        return new Grid(rows);
+    }
+
+    public static Grid emptyGrid(int size){
+        ArrayList<ArrayList<Integer>> rows = new ArrayList<>();
+
+        ArrayList<Integer> emptyRow = new ArrayList<>();
+
+        for(int i = 0; i < size; i++){
+            emptyRow.add(0);
+        }
+        for(int i = 0; i < size; i++){
+            rows.add(emptyRow);
+        }
+
         return new Grid(rows);
     }
 }
