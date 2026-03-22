f5c24b9b2d3f673219d79bf24247cd2e831c509c
StolenGalaxy <56508206+StolenGalaxy@users.noreply.github.com>
2026-03-22 02:42:57 +0000

    Add complete vs valid grid
    Valid grid - all filled cells are valid
    Complete grid - all cells are filled and valid

--- src/main/java/com/stolengalaxy/sudoku_solver/grid/Validation.java
+++ src/main/java/com/stolengalaxy/sudoku_solver/grid/Validation.java
@@ -5,15 +5,22 @@ import com.stolengalaxy.sudoku_solver.util.ArrayTools;
 import java.util.ArrayList;
 
 public class Validation {
-    public static boolean isSetValid(ArrayList<Integer> values){
+    public static boolean isSetValid(ArrayList<Integer> values, ArrayList<Integer> validValues){
         boolean stillValid = true;
 
-        for(int i=1; i <=9; i++){
-                if (ArrayTools.countIntegerOccurrences(values, i) > 1){
-                    stillValid = false;
-                    break;
-                }
+        for(int value:values){
+            if(value == 0){
+                break;
             }
+            if(!validValues.contains(value)){
+                stillValid = false;
+                break;
+            }
+            if (ArrayTools.countIntegerOccurrences(values, value) > 1){
+                stillValid = false;
+                break;
+            }
+        }
         return stillValid;
     }
 
@@ -21,23 +28,27 @@ public class Validation {
         boolean stillValid = true;
 
         for(ArrayList<Integer> row:grid.rows()){
-            if(!isSetValid(row)){
+            if(!isSetValid(row, grid.validValues())){
                 stillValid = false;
                 break;
             }
         }
         for(ArrayList<Integer> column:grid.columns()){
-            if(!isSetValid(column)){
+            if(!isSetValid(column, grid.validValues())){
                 stillValid = false;
                 break;
             }
         }
         for(ArrayList<Integer> block:grid.blocks()){
-            if(!isSetValid(block)){
+            if(!isSetValid(block, grid.validValues())){
                 stillValid = false;
                 break;
             }
         }
         return stillValid;
+    }
+
+    public static boolean isGridComplete(Grid grid){
+        return !grid.values().contains(0) && isGridValid(grid);
     }
 }
