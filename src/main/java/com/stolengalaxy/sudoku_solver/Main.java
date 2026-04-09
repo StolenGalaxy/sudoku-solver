c2e3174e6bea57ea9402e7239ba64a744afadec1
StolenGalaxy <56508206+StolenGalaxy@users.noreply.github.com>
2026-04-09 20:09:15 +0100

    Completed CSV to Grid importer

--- src/main/java/com/stolengalaxy/sudoku_solver/Main.java
+++ src/main/java/com/stolengalaxy/sudoku_solver/Main.java
@@ -1,21 +1,19 @@
 package com.stolengalaxy.sudoku_solver;
 
-import com.stolengalaxy.sudoku_solver.grid.Generator;
 import com.stolengalaxy.sudoku_solver.grid.Grid;
+import com.stolengalaxy.sudoku_solver.grid.Importer;
 import com.stolengalaxy.sudoku_solver.solver.DFS;
 import com.stolengalaxy.sudoku_solver.solver.Heuristics;
 
 public class Main {
     static void main() {
-        //TODO: input sample grids from a file to solve
-        Grid grid = Generator.emptyGrid(9);
-        System.out.println("Starting grid:\n" +  grid + "\n");
+        Grid grid = Importer.fromCSV();
+        System.out.println("Starting grid:\n" + grid + "\n");
 
-        // note this won't currently do anything for the given empty grid
         grid = Heuristics.applyHeuristics(grid);
-        System.out.println("Heuristic-applied grid:\n" +  grid + "\n");
+        System.out.println("Applied heuristics:\n" + grid + "\n");
 
         grid = DFS.complete(grid);
-        System.out.println("DFS-applied grid:\n" +  grid);
+        System.out.println("Applied DFS:\n" + grid);
     }
 }
--- src/main/java/com/stolengalaxy/sudoku_solver/grid/Importer.java
+++ src/main/java/com/stolengalaxy/sudoku_solver/grid/Importer.java
@@ -1,9 +1,12 @@
 package com.stolengalaxy.sudoku_solver.grid;
 
+import com.stolengalaxy.sudoku_solver.cell.Cell;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Importer {
@@ -18,21 +21,29 @@ public class Importer {
     }
     public static Grid fromCSV(){
         File file = new File(pickFile());
-
         try {
             Scanner scanner = new Scanner(file);
-            while(scanner.hasNextLine()){
-                String row = scanner.nextLine();
-                String[] values = row.split(",");
-                for(String value:values){
-                    // for now just print the value
-                    System.out.println(value.strip());
+
+            ArrayList<ArrayList<Cell>> rows = new ArrayList<>();
+            for(int rowIndex = 0; scanner.hasNextLine(); rowIndex++){
+                String line = scanner.nextLine();
+                String[] unformattedValues = line.split(",");
+                ArrayList<Cell> row = new ArrayList<>();
+
+                for(int columnIndex = 0; columnIndex < unformattedValues.length; columnIndex++){
+                    String formattedValue = unformattedValues[columnIndex].strip();
+
+                    Cell cell = new Cell(Integer.parseInt(formattedValue));
+                    cell.row = rowIndex;
+                    cell.column = columnIndex;
+
+                    row.add(cell);
                 }
+                rows.add(row);
             }
+            return new Grid(rows);
         } catch (FileNotFoundException e){
             throw new RuntimeException(e);
         }
-
-        return Generator.emptyGrid(9);
     }
 }
