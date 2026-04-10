15923f6baa0ae43b9463c34bdd0a6e37923ec485
StolenGalaxy <56508206+StolenGalaxy@users.noreply.github.com>
2026-04-10 01:33:14 +0100

    Print remaining empty cells after each stage

--- src/main/java/com/stolengalaxy/sudoku_solver/Main.java
+++ src/main/java/com/stolengalaxy/sudoku_solver/Main.java
@@ -8,13 +8,18 @@ import com.stolengalaxy.sudoku_solver.solver.Heuristics;
 public class Main {
     static void main() {
         Grid grid = Importer.fromCSV();
-        System.out.println("Initial grid:\n" + grid + "\n");
+        int initialEmptyCells = grid.emptyCells().size();
+        System.out.println("Initial grid contains " + initialEmptyCells + " empty cells:\n" + grid);
 
         Grid modifiedGrid = Heuristics.completeHeuristics(grid);
-        System.out.println("Heuristics applied:\n" + modifiedGrid + "\n");
+        int postHeuristicEmptyCells = modifiedGrid.emptyCells().size();
+        System.out.println("\nHeuristics applied, successfully solving " + (initialEmptyCells - postHeuristicEmptyCells) + " empty cells:\n" + modifiedGrid);
 
-        modifiedGrid = DFS.complete(modifiedGrid);
-        System.out.println("DFS applied:\n" + modifiedGrid);
-
+        if(postHeuristicEmptyCells > 0){
+            System.out.println("\nStarting DFS. There are " + postHeuristicEmptyCells + " empty cells remaining.");
+            modifiedGrid = DFS.complete(modifiedGrid);
+            System.out.println("\nDFS applied, successfully solving " + (postHeuristicEmptyCells) + " empty cells:\n" + modifiedGrid);
+        }
+        System.out.println("Solving complete!");
     }
 }
